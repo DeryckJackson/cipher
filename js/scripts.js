90d92f9f31ddec1cc52cb066dc18b3f776e70ff7
Deryck Jackson <44139628+DeryckJackson@users.noreply.github.com>
2020-06-01 16:06:27 -0700

    something is wrong with the cipher logic in scripts.js
    Co-authored-by: Ian Gregg <iangregg188@gmail.com>

--- js/scripts.js
+++ js/scripts.js
@@ -1,41 +1,46 @@
+jQuery(document).ready(function() {
 const capitalizeLetters = function (str) {
-  const char1 = str[0].toUpperCase();
-  const char2 = str[str.length - 1].toUpperCase();
-  return char1 + char2
-}
-
-
-const reverseLetters = function(str) {
-  const char1 = str[0];
-  const char2 = str[str.length - 1];
-  return char2 + char1
-}
-
-const letters = function(str) {
-  let funcCall1 = capitalizeLetters(str);
-  let funcCall2 = reverseLetters(funcCall1);
-  return funcCall2
-}
-
-const countLetters = function(str) {
-  let count = 0;
-  for (let i = 0; i < str.length; i++) {
-    count++;
+    const char1 = str[0].toUpperCase();
+    const char2 = str[str.length - 1].toUpperCase();
+    return char1 + char2
   }
 
-  let selectMiddleChar = str[Math.Floor(count)];
+  const reverseLetters = function(str) {
+    const char1 = str[0];
+    const char2 = str[str.length - 1];
+    return char2 + char1
+  }
 
-  return selectMiddleChar
-}
+  const letters = function(str) {
+    let funcCall1 = capitalizeLetters(str);
+    let funcCall2 = reverseLetters(funcCall1);
+    return funcCall2
+  }
 
-let userInput = prompt("Please enter a sentence:");
+  const countLetters = function(str) {
+    let count = 0;
+    for (let i = 0; i < str.length; i++) {
+      count++;
+    }
 
-// I am a sentence ---> sI am a sentenceEI
-const reversedLetters = letters(userInput);
-const middleLetter = countLetters(userInput);
-const newUserStr = middleLetter + userInput + reversedLetters;
+    let selectMiddleChar = str[Math.Floor(count)];
 
+    return selectMiddleChar
+  }
 
-jQuery("h1").click(function() {
-  alert("newUserStr");
+  jQuery("h1").click(function() {
+    alert("newUserStr");
+  });
+
+  $("img").click(function() {
+    alert("You clicked an image.")
+  })
+
+  let userInput = prompt("Please enter a sentence:");
+
+  // const userInput = "I am a sentence"
+  // I am a sentence ---> sI am a sentenceEI
+  // const reversedLetters = letters(userInput);
+  // const middleLetter = countLetters(userInput);
+  // const newUserStr = middleLetter + userInput + reversedLetters;
 });
